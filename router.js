86bc0ecb325a5515ae68cf7be74e9a20dce42e9b
leglars <leglars@gmail.com>
2015-06-18 22:44:23 +1000

    create a server to handle post data submit.

--- router.js
+++ router.js
@@ -2,10 +2,10 @@
  * Created by leglars on 18/06/15.
  */
 
-function route (handle, pathname, response) {
+function route (handle, pathname, response, postData) {
     console.log("About to route a request for " + pathname);
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response);
+        handle[pathname](response, postData);
     } else {
         console.log("No request handle found for " + pathname);
         response.writeHead(404, {"Content-Type": "text/plain"});
